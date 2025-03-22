a703103ed53843a3df8d102e8a79d11a1245a1dd
Jóhann Friðriksson <johann.fridriksson@gmail.com>
2025-03-22 00:49:42 +0000

    indentation-fix in jofinstall.sh

--- bin/bin/jofinstall.sh
+++ bin/bin/jofinstall.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 packagename="${1}"
-sudo sed -i "/#jofinstall/a \ \ \ \ \ \ ${packagename}" /etc/nixos/jof-packages.nix
+sudo sed -i "/#jofinstall/a \ \ \ \ ${packagename}" /etc/nixos/jof-packages.nix
 sudo nixos-rebuild switch
 pushd /etc/nixos
 git add jof-packages.nix
