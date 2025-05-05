868157eb86734af65a178253262a1edcc6222b98
Jóhann Friðriksson <johann.fridriksson@gmail.com>
2025-05-05 14:29:20 +0000

    Switched my app-utils to Firefox

--- bin/bin/messenger.sh
+++ bin/bin/messenger.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-vivaldi --app="https://messenger.com" &!
+#vivaldi --app="https://messenger.com" &!
+firefox 'moz-extension://67f5a6ad-c471-408d-bd2b-20e2c823a435/ssb.html?url=https=-=3A=-=2F=-=2Fmessenger.com&name=Messenger&incognito=false'
