088a767252b312fe67771d607eee601e49ea929d
Jóhann Friðriksson <johann@tern.is>
2025-05-05 14:29:20 +0000

    Switched my app-utils to Firefox

--- bin/bin/chat.sh
+++ bin/bin/chat.sh
@@ -1,2 +1,3 @@
 #!/usr/bin/env bash
-vivaldi --app="https://chat.google.com" &!
+#vivaldi --app="https://chat.google.com" &!
+firefox 'moz-extension://67f5a6ad-c471-408d-bd2b-20e2c823a435/ssb.html?url=https=-=3A=-=2F=-=2Fchat.google.com&name=TernChat&incognito=false' &!
